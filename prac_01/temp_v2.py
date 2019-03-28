a6712142a0d6f0ff7372759a2a676560ea59bae0
BrodyJorgensen <jorgo9110@gmail.com>
2019-03-28 13:45:47 +1000

    Add prac 1 files

--- prac_01/temp_v2.py
+++ prac_01/temp_v2.py
@@ -3,30 +3,32 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 MENU = """C - Convert Celsius to Fahrenheit
-    F - Convert Fahrenheit to Celsius
-    Q - Quit"""
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
 
 
 def main():
     print(MENU)
-    choice = input(" ").upper()
-    while choice!= "Q":
-    if choice == "c":
-        celsius = float(input("celsius: "))
-        farnheight = celsius_tofahrenheit_con(celsius)
-        print("results: {:.2f] C".format(farnheight))
-    elif choice == "F":
-        farnheight = float(input("Farnheight: "))
-        celsius =farnheight_to_celsius_conversion(farnheight)
-        print("result: {:.2f} C".format(celsius))
-    else:
-        print("invald option")
-    print(MENU)
+    choice = input("Enter choice: ").upper()
+    while choice != "Q":
+        if choice == "c":
+            celsius = float(input("celsius: "))
+            fahrenheit = celsius_to_fahrenheit_con(celsius)
+            print("results: {:.2f] C".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Farnheight: "))
+            celsius = fahrenheit_to_celsius_conversion(fahrenheit)
+            print("result: {:.2f} C".format(celsius))
+        else:
+            print("invald option")
+            print(MENU)
 
-def celsius_tofahrenheit_con(celsius):
+
+def celsius_to_fahrenheit_con(celsius):
     return celsius * 9.0 / 5 + 32
 
-def farnheight_to_celsius_conversion(fahrenheit):
+
+def fahrenheit_to_celsius_conversion(fahrenheit):
     return 5 / 9 * (fahrenheit - 32)
 
 
